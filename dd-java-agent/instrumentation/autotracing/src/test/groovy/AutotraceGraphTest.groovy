3454aa0879933386ccd5eaee44e2747b698f5f7a
Andrew Kent <andrew.kent@datadoghq.com>
2018-09-26 19:47:04 -0700

    Fix test

--- dd-java-agent/instrumentation/autotracing/src/test/groovy/AutotraceGraphTest.groovy
+++ dd-java-agent/instrumentation/autotracing/src/test/groovy/AutotraceGraphTest.groovy
@@ -2,16 +2,19 @@ import datadog.trace.agent.test.AgentTestRunner
 import datadog.trace.agent.tooling.Utils
 import datadog.trace.bootstrap.autotrace.AutotraceGraph
 import datadog.trace.bootstrap.autotrace.AutotraceNode
+import datadog.trace.bootstrap.autotrace.GraphMutator
+
+import java.util.concurrent.TimeUnit
 
 class AutotraceGraphTest extends AgentTestRunner {
   def "unique edges only added once" () {
     setup:
-    AutotraceGraph graph = new AutotraceGraph(Utils.getBootstrapProxy(), instrumentation, 10, 1)
+    AutotraceGraph graph = new AutotraceGraph(Utils.getBootstrapProxy(), new NoOpMutator(), TimeUnit.NANOSECONDS.convert(10, TimeUnit.MILLISECONDS), TimeUnit.NANOSECONDS.convert(1, TimeUnit.MILLISECONDS))
     AutotraceNode node1 = graph.getNode(null, "Foo", "bar", true)
     AutotraceNode node2 = graph.getNode(null, "Foo", "baz", true)
     AutotraceNode node3 = graph.getNode(null, "Foo", "baz", true)
-    node1.addEdges([node2, node3, node3, node2])
-    node1.addEdges([node3])
+    node1.addEdges(node2, node3, node3, node2)
+    node1.addEdges(node3)
 
     expect:
     node1.getEdges().size() == 1
@@ -19,5 +22,17 @@ class AutotraceGraphTest extends AgentTestRunner {
 
   def "no strong classloader refs held" () {
     // TODO
+  }
+
+  class NoOpMutator implements GraphMutator {
+
+    @Override
+    void updateBytecode(AutotraceNode node) {
+      node.markBytecodeUpdated()
+    }
+
+    @Override
+    void awaitUpdates() {
+    }
   }
 }
